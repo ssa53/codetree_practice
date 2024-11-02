2e6651cdf988de46a2a2131fcbee61348a23765c
ssa53 <ssasodoso53@gmail.com>
2024-11-02 20:02:49 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241102/DateTime to DateTime/datetime-to-datetime.py
+++ 241102/DateTime to DateTime/datetime-to-datetime.py
@@ -1,5 +1,9 @@
 a,b,c = map(int,input().split())
-if a <= 11 and b <=11 and c < 11:
+if a < 11 : 
+    print(-1)
+elif a==11 and b< 11:
+    print(-1)
+elif a==11 and b==11 and c<11:
     print(-1)
 
 def get_min(day,hour,minute):

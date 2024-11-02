177e2d87e22a9a29ae05feaaaed0acae58991c28
ssa53 <ssasodoso53@gmail.com>
2024-11-02 20:03:40 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241102/DateTime to DateTime/datetime-to-datetime.py
+++ 241102/DateTime to DateTime/datetime-to-datetime.py
@@ -1,10 +1,4 @@
 a,b,c = map(int,input().split())
-if a < 11 : 
-    print(-1)
-elif a==11 and b< 11:
-    print(-1)
-elif a==11 and b==11 and c<11:
-    print(-1)
 
 def get_min(day,hour,minute):
     return (day-1)*24*60 + hour*60 + minute
@@ -12,4 +6,7 @@ def get_min(day,hour,minute):
 start = get_min(11,11,11)
 end = get_min(a,b,c)
 
-print(end-start)
+if end-start >= 0:
+    print(end-start)
+else:
+    print(-1)

9213480cf3d0c521a6e459d5567842ef422c17fb
ssa53 <ssasodoso53@gmail.com>
2024-11-03 17:58:04 +0900

    Title: 흰검 칠하기, Time: 85ms, Memory: 22MB, Status: Runtime Error - CodeTree

--- 241103/흰검 칠하기/painting-white-black.py
+++ 241103/흰검 칠하기/painting-white-black.py
@@ -1,42 +1,39 @@
-n = int(input())
-OFFSET = 100000
-MAX = 200000
-segment = []
-start = 0
+MAX = 100000
+
+n= int(input())
+lst = [0] * 200001
+cnt_black = [0] * 200001
+cnt_white = [0] * 200001
+black,white,gray = 0,0,0
+
+cur = MAX
 for i in range(n):
-    distance,direction = input().split()
-    distance = int(distance)
-    if direction == "L":
-        left = start - distance
-        right = start
-        start = start - distance
-        segment.append([left,right])
-    else:
-        left = start
-        right = start + distance
-        start = start + distance
-        segment.append([left,right])
-checked = [[0,"color"]] * (MAX+1)
-for x,y in segment:
-	x,y = x+OFFSET,y+OFFSET
-	if y > x : 
-		for i in range(x,y):
-			checked[i][0] += 1000
-			checked[i][1] = "black"
-	elif y < x: 
-		for i in range(y,x,-1):
-			checked[i][0] += 1
-			checked[i][1] = "white"
-white = 0
-gray = 0
-black = 0
-for i in checked:
-	if i[0]//1000 >= 2 and i[0]%1000 >= 2:
+	dist, dir = input().split()
+	dist = int(dist)
+	if dir == "L":
+		while dist>0:
+			lst[cur] = 1 
+			cnt_white[cur] += 1
+			dist -= 1
+
+			if dist:
+				cur -= 1 
+	else:
+		while dist>0:
+			lst[cur] = 2
+			cnt_black += 1
+			dist -= 1
+			
+			if dist:
+				cur+=1
+
+for i in range(200001):
+	if cnt_black[i]>=2 and cnt_white[i] >=2:
 		gray += 1
-	else: 
-		if i[1] == "black":
-			black += 1
-		elif i[1] == "white":
-			white += 1 
+	elif lst[i] == 1:
+		white += 1
+	elif lst[i] == 2:
+		black += 1
+			
 
 print(white,black,gray)

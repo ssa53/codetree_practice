099e2cdcec44fb08226ab7a6cbcf4bea4c8ec691
ssa53 <ssasodoso53@gmail.com>
2024-11-03 15:47:45 +0900

    Title: 블럭쌓는 명령2, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241103/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241103/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,12 +1,13 @@
 n,k=map(int,input().split())
 
 lst = [
-    0 for i in range(n):
+    0 for i in range(n)
 ]
 
 for i in range(k):
     a,b = map(int,input().split())
-    lst[a,b+1] += 1
+    for j in range(a,b+1):
+        lst[j] +=1
 
 max= 0
 for i in lst:

c8f985643a020bfc543b690e94fb2eddae1bd3f7
ssa53 <ssasodoso53@gmail.com>
2024-11-03 15:16:16 +0900

    Title: 10진수로 변환하기, Time: 42ms, Memory: 15MB, Status: Passed - CodeTree

--- 241103/10진수로 변환하기/convert-to-decimal.py
+++ 241103/10진수로 변환하기/convert-to-decimal.py
@@ -1,7 +1,7 @@
 binary = []
 n = str(input())
 for i in range(len(n)):
-    binary.append(n[i])
+    binary.append(int(n[i]))
 sum=0
 for i in range(len(binary)):
     sum += binary[i]*pow(2,len(binary)-1-i)

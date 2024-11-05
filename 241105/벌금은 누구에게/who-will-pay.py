fa97a930090bd61d2a64b232383db632a466ee23
ssa53 <ssasodoso53@gmail.com>
2024-11-05 17:04:46 +0900

    Title: 벌금은 누구에게, Time: 79ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241105/벌금은 누구에게/who-will-pay.py
+++ 241105/벌금은 누구에게/who-will-pay.py
@@ -3,10 +3,17 @@ lst_student = [
     0 
     for i in range(101)
 ]
+penalized_person = [
+    int(input())
+    for i in range(m)
+]
 #lst_student = [ 0 0 0 0 0 0 0 0 0 0 0 0 0 0........ ]
-for i in range(n):
-    idx = int(input())
-    lst_student[idx] += 1 
-    for j in range(len(lst_student)):
-        if lst_student[j] == m:
-            print(j)
+ans = 0
+for target in penalized_person:
+    lst_student[target] += 1 
+    
+    if lst_student[target]>=k:
+        ans = target 
+        break 
+
+print(ans)

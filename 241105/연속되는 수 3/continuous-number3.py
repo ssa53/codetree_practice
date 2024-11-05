824f598ed5b3cafdb0813032133de0b307439b71
ssa53 <ssasodoso53@gmail.com>
2024-11-05 15:08:40 +0900

    Title: 연속되는 수 3, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241105/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = [
+    int(input())
+    for i in range(n):
+]
+#부호 (lst[i]<0 or lst[i]>0) 이 같은거중에 길이(cnt)가 제일 큰거..
+ans,cnt =0, 0
+for i in range(n):
+    if i>=1 and lst[i]*lst[i-1] < 0 :
+        cnt += 1
+    else:
+        cnt = 1 
+    ans = max(ans,cnt)
+
+print(ans)

cfb6fb7f179c117a698c4b903cb589ba03527068
ssa53 <ssasodoso53@gmail.com>
2024-11-05 17:05:27 +0900

    Title: 벌금은 누구에게, Time: 63ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/벌금은 누구에게/who-will-pay.py
+++ 241105/벌금은 누구에게/who-will-pay.py
@@ -1,14 +1,14 @@
 n,m,k=map(int,input().split())
 lst_student = [
     0 
-    for i in range(101)
+    for i in range(n+1)
 ]
 penalized_person = [
     int(input())
     for i in range(m)
 ]
 #lst_student = [ 0 0 0 0 0 0 0 0 0 0 0 0 0 0........ ]
-ans = 0
+ans = -1
 for target in penalized_person:
     lst_student[target] += 1 
     

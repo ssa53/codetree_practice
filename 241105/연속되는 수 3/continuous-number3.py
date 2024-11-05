0cb0a66fb422be7c2f55cfc811910f0fb1cadaec
ssa53 <ssasodoso53@gmail.com>
2024-11-05 15:10:40 +0900

    Title: 연속되는 수 3, Time: 51ms, Memory: 15MB, Status: Passed - CodeTree

--- 241105/연속되는 수 3/continuous-number3.py
+++ 241105/연속되는 수 3/continuous-number3.py
@@ -6,7 +6,7 @@ lst = [
 #부호 (lst[i]<0 or lst[i]>0) 이 같은거중에 길이(cnt)가 제일 큰거..
 ans,cnt =0, 0
 for i in range(n):
-    if i>=1 and lst[i]*lst[i-1] < 0 :
+    if i>=1 and lst[i]*lst[i-1] > 0 :
         cnt += 1
     else:
         cnt = 1 

630cc109eab1aaf760e88aaee31d949789c4a5b9
ssa53 <ssasodoso53@gmail.com>
2024-11-06 14:58:54 +0900

    Title: 선두를 지켜라, Time: 89ms, Memory: 33MB, Status: Runtime Error - CodeTree

--- 241106/선두를 지켜라/keep-the-lead.py
+++ 241106/선두를 지켜라/keep-the-lead.py
@@ -1,16 +1,16 @@
 MAX = 1000000
-n,m=map(int,input().split())
+n,m=tuple(map(int,input().split()))
 arr_a,arr_b = [0]*(MAX+1),[0]*(MAX+1)
 time_a = 1
 for i in range(n):
-    v,t=map(int,input().split())
+    v,t=tuple(map(int,input().split()))
     for j in range(t):
         arr_a[time_a] = arr_a[time_a - 1] + v
         time_a += 1
 
 time_b = 1
 for i in range(n):
-    v,t=map(int,input().split())
+    v,t=tuple(map(int,input().split()))
     for j in range(t):
         arr_b[time_b] = arr_b[time_b - 1] + v
         time_b += 1

dca70c2d5821b6d40de6a67eddc6bad9d1da9411
ssa53 <ssasodoso53@gmail.com>
2024-11-06 15:51:42 +0900

    Title: 좌우로 움직이는 로봇, Time: 58ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 241106/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,34 @@
+MAX = 1000000
+n,m=map(int,input().split())
+arr_a , arr_b = [0]*(MAX+1) , [0]*(MAX+1)
+time_a = 1
+for i in range(n):
+    t,d = input().split()
+    t = int(t)
+    if d=='R':
+        for j in range(t):
+            arr_a[time_a] = arr_a[time_a-1] + 1
+            time_a += 1
+    elif d=='L':
+        for j in range(t):
+            arr_a[time_a] = arr_a[time_a-1] - 1
+            time_a += 1
+
+time_b = 1
+for i in range(m):
+    t,d = input().split()
+    t = int(t)
+    if d=='R':
+        for j in range(t):
+            arr_b[time_b] = arr_b[time_b-1] + 1
+            time_b += 1
+    elif d=='L':
+        for j in range(t):
+            arr_b[time_b] = arr_b[time_b-1] - 1
+            time_b += 1
+cnt = 0
+for i in range(1,max(time_b,time_a)+1):
+    if arr_a[i] == arr_b[i] :
+        if arr_a[i-1] != arr_b[i-1]:
+            cnt += 1
+print(cnt)

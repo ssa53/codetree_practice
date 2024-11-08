b04f0bfeffee0f8e627a753ac89bf47f9d3a302e
ssa53 <ssasodoso53@gmail.com>
2024-11-08 15:20:58 +0900

    Title: 알파벳에 따른 평가 2, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241108/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,13 @@
+s = str(input())
+if s == 'S':
+    print('Superior')
+elif s == 'F':
+    print('Fantastic')
+elif s == 'G' :
+    print('Good')
+elif s == 'U':
+    print('Usually')
+elif s == 'E':
+    print('Effort')
+else:
+    print("Failure")

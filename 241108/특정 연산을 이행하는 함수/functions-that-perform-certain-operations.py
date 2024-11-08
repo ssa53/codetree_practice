bf9dcbdfd23b38ba29ec660f68634bc94c9f8183
ssa53 <ssasodoso53@gmail.com>
2024-11-08 15:24:10 +0900

    Title: 특정 연산을 이행하는 함수, Time: 45ms, Memory: 15MB, Status: Passed - CodeTree

--- 241108/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.py
+++ 241108/특정 연산을 이행하는 함수/functions-that-perform-certain-operations.py
@@ -1,7 +1,8 @@
 a,b,c = map(int,input().split())
 def magic(n):
     if n%2 == 0:
-        return n/2
-    return (n*3)-20
+        return int(n/2)
+    else:
+        return (n*3)-20
 
 print(magic(a),magic(b),magic(c))

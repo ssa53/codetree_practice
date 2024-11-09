8e8c4825c835e0d0d83e0f72dc6030d518bde295
ssa53 <ssasodoso53@gmail.com>
2024-11-09 15:14:35 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 45ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241109/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241109/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -2,23 +2,21 @@
 #t는 시각,x,y는 악수하는 사람idx 
 n,k,p,T=map(int,input().split())
 people = [0]*(n)
-people[p] = 1
-time = []
+people[p-1] = 1
 lst = []
 for i in range(T):
     t,x,y= map(int,input().split())
-    time.append(t)
-    lst.append([x,y])
+    lst.append([t,x,y])
 
-time.sort()
+lst.sort(key=lambda x: x[0])
+
 for i in range(k):
-    for j in range(2):
-        if lst[i][0] == p:
-            people[lst[i][1]] = 1 
-        elif lst[i][1] == p :
-            people[lst[i][0]] = 1 
-        else: 
-            pass
+    if lst[i][1] == p :
+        people[lst[i][2]-1] = 1
+    elif lst[i][2] == p :
+        people[lst[i][1]-1] = 1
+    else:
+        pass
 
 for i in people:
     print(i,end="")

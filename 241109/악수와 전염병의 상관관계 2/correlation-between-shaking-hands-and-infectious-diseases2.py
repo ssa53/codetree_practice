d5e6d3d3e327f4bc08ebdf283dd1c07cd68b1d67
ssa53 <ssasodoso53@gmail.com>
2024-11-09 15:20:04 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241109/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 241109/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,22 +1,38 @@
-#n은 개발자 수 k는 감염가능한 횟수 p는 처음 감염된 개발자의 idx T는 반복횟수 
-#t는 시각,x,y는 악수하는 사람idx 
-n,k,p,T=map(int,input().split())
-people = [0]*(n)
-people[p-1] = 1
-lst = []
-for i in range(T):
-    t,x,y= map(int,input().split())
-    lst.append([t,x,y])
+class Shake :
+    def __init__(self,time,person1,person2):
+        self.time,self.person1,self.person2 = time,person1,person2
+    
+n,k,p,t = map(int,input().split())
+shakes = []
+for i in range(t):
+    time,person1,person2 = map(int,input().split())
+    shakes.append(Shake(time,person1,person2))
 
-lst.sort(key=lambda x: x[0])
+shake_num = [0] * (n+1)
+infected = [False] * (n+1)
 
-for i in range(k):
-    if lst[i][1] == p :
-        people[lst[i][2]-1] = 1
-    elif lst[i][2] == p :
-        people[lst[i][1]-1] = 1
+infected[p] = True
+
+shakes.sort(key = lambda x : x.time)
+
+for shake in shakes:
+    target1 = shake.person1
+    target2 = shake.person2
+    if infected[target1]:
+	    shake_num[target1] += 1
+	if infected[target2]:
+		shake_num[target2] += 1
+	
+	# target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+	if shake_num[target1] <= k and infected[target1]:
+		infected[target2] = True
+	
+	# target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+	if shake_num[target2] <= k and infected[target2]:
+		infected[target1] = True
+
+for i in range(1,n+1):
+    if infected[i]:
+        print(1,end="")
     else:
-        pass
-
-for i in people:
-    print(i,end="")
+        print(0,end="")

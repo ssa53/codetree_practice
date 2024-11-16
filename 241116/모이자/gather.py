216db39329b8a08cf836e57eb05da35986d66a5e
ssa53 <ssasodoso53@gmail.com>
2024-11-16 18:50:10 +0900

    Title: 모이자, Time: 92ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 241116/모이자/gather.py
@@ -0,0 +1,22 @@
+import sys
+n = int(input())
+lst = list(map(int,input().split()))
+result = []
+# lst 에는 lst[0] = 1 0번지에 1명이 산다. 
+for i in range(len(lst)):
+    sum=0
+    for j in range(len(lst)):
+        distance = j-i
+        if j-i <0 : 
+            distance = i-j
+        sum += distance * (lst[j]) 
+        if sum < INT_MAX : 
+            INT_MAX = sum 
+    result.append(sum)
+print(min(result))
+
+
+            
+        
+        
+        

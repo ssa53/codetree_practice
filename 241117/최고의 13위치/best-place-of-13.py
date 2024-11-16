49db10f7cf42a309427d1806790e0fffa5a5080d
ssa53 <ssasodoso53@gmail.com>
2024-11-17 00:50:46 +0900

    Title: 최고의 13위치, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241117/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for i in range(n)
+]
+ans = 0
+for i in range(n):
+    sum = 0
+    for j in range(n-2):
+        sum += arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        ans = max(sum,ans) 
+
+print(ans)

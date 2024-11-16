4f2f562e5d36e3187aae611bb4f460dbf0f3c1eb
ssa53 <ssasodoso53@gmail.com>
2024-11-16 18:56:48 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 51ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241116/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,9 @@
+a = list(str(input()))
+cnt = 0
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        if a[i] == '(':
+            if a[j] == ')':
+                cnt += 1 
+print(cnt)
+            

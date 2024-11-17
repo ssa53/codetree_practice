a055c398be5728e66988521aec3f3b5ec028b00b
ssa53 <ssasodoso53@gmail.com>
2024-11-17 19:12:05 +0900

    Title: 원 모양으로 되어있는 방, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241117/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,21 @@
+n = int(input())
+lst = [
+    int(input()) 
+    for i in range(n)
+]
+print(lst)
+sum_lst = []
+for i in range(n):
+    sum = 0
+    for j in range(n):
+        diff = j-i
+        if j-i < 0:
+            diff = 5-abs(j-i)
+        dist = lst[j]*diff
+        sum += dist
+    sum_lst.append(sum)
+print(min(sum_lst))
+ 
+
+
+    

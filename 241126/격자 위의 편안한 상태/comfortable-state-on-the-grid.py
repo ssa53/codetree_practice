97690116152a0232ddf8aaef7fada8df0bbeb4ff
ssa53 <ssasodoso53@gmail.com>
2024-11-26 14:32:01 +0900

    Title: 격자 위의 편안한 상태, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241126/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,23 @@
+n,m=map(int,input().split())
+arr = [
+    [0]*n
+    for i in range(n)
+]
+def count(r,c):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    
+    return cnt 
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
+    
+for i in range(m):
+    r,c=map(int,input().split())
+    arr[r-1][c-1] = 1 
+    if count(arr,r-1,c-1) == 3:
+        print(1)
+    else:
+        print(0)

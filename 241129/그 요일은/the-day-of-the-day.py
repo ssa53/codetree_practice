0ac463bdb1e03b63088f8bbb094c447d9acb7918
ssa53 <ssasodoso53@gmail.com>
2024-11-29 16:32:21 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241129/그 요일은/the-day-of-the-day.py
+++ 241129/그 요일은/the-day-of-the-day.py
@@ -1,18 +1,35 @@
-months = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-m1,d1,m2,d2=map(int,input().split())
-what_yoil = str(input())
-result = 0 
-if m2-m1 > 1:
-    result = months[m1-1]-d1 + d2 
-    for i in range(m1+1,m2):
-        result += months[i-1] 
-elif m2-m1 == 1:
-    result = months[m1-1]-d1 + d2 
-elif m2==m1 : 
-    result = d2-d1 
+m1, d1, m2, d2 = map(int, input().split())
+day = input()
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+days_dict = {days:idx for idx, days in enumerate(days)}
+idx = days_dict[day]
 
-cnt = result//7 
-result /= 7
-if result :
-    cnt += 1 
-print(cnt)
+def calcDate(m, d):
+    sum2 = 0
+    for i in range(1, m):
+        if i >= 8:
+            if i % 2 == 0:
+                sum2 += 31
+            else:
+                sum2 += 30
+        else:
+            if i % 2 == 0:
+                if i == 2:
+                    sum2 += 29
+                else:
+                    sum2 += 30
+            else:
+                sum2 += 31
+    sum2 += d
+    return sum2
+
+sum1 = calcDate(m1, d1)
+sum2 = calcDate(m2, d2)
+
+sum0 = sum2-sum1
+
+count = sum0//7
+if sum0%7 >= idx:
+    count += 1
+
+print(count)

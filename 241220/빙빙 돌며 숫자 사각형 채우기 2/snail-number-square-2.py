15f1dbf4a6a86b42972a24eae699614e33844d29
ssa53 <ssasodoso53@gmail.com>
2024-12-20 10:32:39 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 92ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241220/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,30 @@
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+n,m=map(int,input().split())
+arr = [
+    [0]*m
+    for i in range(n)
+]
+
+dxs,dys = [0,1,0,-1],[1,0,-1,0]
+x,y=0,0
+dir_num = 1 
+
+arr[x][y] = 1 
+for i in range(2,n*m+1):
+    while True:
+        nx,ny = x+dxs[dir_num],y+dys[dir_num]
+        if in_range(nx,ny) and arr[nx][ny] == 0:
+            x,y=nx,ny
+            arr[x][y] = i
+            break 
+        else:
+            dir_num = (dir_num+3)%4
+
+for i in ragne(n):
+    for j in ragne(m):
+        print(arr[i][j],end=' ')
+    print()
+    
+    

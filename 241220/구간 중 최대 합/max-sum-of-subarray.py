bafba9c2dc535ab59166ec2292e427f8adad58d5
ssa53 <ssasodoso53@gmail.com>
2024-12-20 17:27:32 +0900

    Title: 구간 중 최대 합, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241220/구간 중 최대 합/max-sum-of-subarray.py
+++ 241220/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,7 +3,10 @@ lst = list(map(int,input().split()))
 sum_lst = []
 for i in range(n-k+1):
     sum = 0
-    sum += lst[i]
+    for j in range(i,i+k):
+        sum += lst[j]
     sum_lst.append(sum)
 print(max(sum_lst))
+
+        
 

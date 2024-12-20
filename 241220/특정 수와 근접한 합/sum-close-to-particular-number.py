f1ddc6ea8ecc19d8615ec4631e28a4dc1886665d
ssa53 <ssasodoso53@gmail.com>
2024-12-20 17:06:25 +0900

    Title: 특정 수와 근접한 합, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241220/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 241220/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,15 +1,15 @@
 n,s=map(int,input().split())
 lst = list(map(int,input().split()))
 sum_lst = []
+SUM = 0
+for k in lst:
+    SUM += k
 for i in range(len(lst)):
     sum = 0 
     for j in range(i+1,len(lst)):
-        sum += lst[i]+lst[j]
-    sum -= s 
-    sum_lst.append(sum)
-for i in range(len(sum_lst)):
-    if sum_lst[i] < 0:
-        sum_lst[i] = -sum_lst[i]
+        sum = lst[i]+lst[j]
+        sum_lst.append(SUM-sum-s)
+
 print(min(sum_lst))
         
         

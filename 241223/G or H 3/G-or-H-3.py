bfc3163c7b361b5d5c6cd0415808a9d4bacfedfe
ssa53 <ssasodoso53@gmail.com>
2024-12-23 19:56:32 +0900

    Title: G or H 3, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241223/G or H 3/G-or-H-3.py
@@ -0,0 +1,27 @@
+n,k=map(int,input().split())
+lst = [
+    input().split()
+    for _ in range(n)
+]
+num_lst = []
+for i in range(n):
+    num_lst.append(int(lst[i][0]))
+arr = [0]*max(num_lst)
+for i in range(n):
+    if lst[i][1] == 'G':
+        arr[int(lst[i][0])-1] = 1
+    elif lst[i][1] == 'H':
+        arr[int(lst[i][0])-1] = 2 
+sum_lst = []
+
+for i in range(len(arr)-k-1):
+    sum = 0
+    if i+k+1 > len(arr):
+        rng = len(arr)
+    else:
+        rng = i+k+1
+    for j in range(i,rng):
+        sum += arr[j]
+    sum_lst.append(sum)
+
+print(max(sum_lst))

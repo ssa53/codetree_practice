c584c644041ebb90837439dd2e2f97e5c7f6570d
ssa53 <ssasodoso53@gmail.com>
2024-12-27 10:49:39 +0900

    Title: G or H 2, Time: 288ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241227/G or H 2/G-or-H-2.py
@@ -0,0 +1,29 @@
+MAX=10000
+n = int(input())
+arr = [0]*(MAX+1)
+
+for _ in range(n):
+    x,c=tuple(input().split())
+    x=int(x)
+    
+    arr[x] = 1 if c=='G' else 2 
+
+max_len = 0 
+for i in range(MAX+1):
+    for j in range(i+1,MAX+1):
+        if arr[i]==0 or arr[j]==0:
+            continue 
+        cnt_g = 0
+        cnt_h = 0
+
+        for k in range(i,j+1):
+            if arr[k]==1:
+                cnt_g+=1
+            if arr[k]==2:
+                cnt_h+=1 
+
+        if cnt_g == 0 or cnt_h==0 or cnt_g==cnt_h:
+            leng=j-i
+            max_len = max(max_len,leng)
+
+print(max_len)

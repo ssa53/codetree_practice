f14a88421ccb0c3a13ecca393fccd1181d7243e8
ssa53 <ssasodoso53@gmail.com>
2025-02-11 11:55:28 +0900

    Title: 거품 정렬 구현, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250211/거품 정렬 구현/implement-bubble-sort.py
+++ 250211/거품 정렬 구현/implement-bubble-sort.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
  for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
                 

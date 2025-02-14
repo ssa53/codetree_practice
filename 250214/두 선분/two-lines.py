0ff533338c21aa62779ef49c50874be23727e916
ssa53 <ssasodoso53@gmail.com>
2025-02-14 13:32:13 +0900

    Title: 두 선분, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/두 선분/two-lines.py
+++ 250214/두 선분/two-lines.py
@@ -1,8 +1,8 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-if x2>=x3:
+if x2>=x3 and x3>=x1:
     print("intersecting")
-elif x4>= x1:
+elif x4>= x1 and x1>=x3:
     print("intersecting")
 else:
     print("nonintersecting")

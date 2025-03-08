fb11da58498bfc2685f0fa848ef9be13b2b683f0
ssa53 <ssasodoso53@gmail.com>
2025-03-08 14:26:42 +0900

    Title: 특정 수 도달하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250308/특정 수 도달하기/reaching-specific-number.py
@@ -0,0 +1,11 @@
+lst=list(map(int,input().split()))
+cnt = 0 
+sum = 0
+for i in lst:
+    if i<250:
+        cnt += 1 
+        sum += i
+    if i>= 250:
+        break
+print(sum,sum/cnt)
+        

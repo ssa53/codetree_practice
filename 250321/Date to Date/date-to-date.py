290fd436898f527dcb1fea45523b5d10318e3c3f
ssa53 <ssasodoso53@gmail.com>
2025-03-21 13:21:39 +0900

    Title: Date to Date, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250321/Date to Date/date-to-date.py
+++ 250321/Date to Date/date-to-date.py
@@ -1,6 +1,6 @@
 def get_days(m,d):
     num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    days=0
+    days=1
     for i in range(1,m):
         days += num_of_days[i]
     days += d

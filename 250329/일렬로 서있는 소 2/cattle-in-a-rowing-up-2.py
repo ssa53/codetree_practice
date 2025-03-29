e6951c6556477f5ed820168d6397473c628214ac
ssa53 <ssasodoso53@gmail.com>
2025-03-29 15:59:35 +0900

    Title: 일렬로 서있는 소 2, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250329/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250329/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,7 +5,7 @@ cnt = 0
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            if i<j and j<k and a[i]<a[j] and a[j]<a[k]:
+            if i<=j and j<=k and a[i]<=a[j] and a[j]<=a[k]:
                 cnt += 1 
 
 print(cnt)

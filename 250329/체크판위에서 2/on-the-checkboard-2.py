7519252381b1435950cedc3c17ca364a74d2772a
ssa53 <ssasodoso53@gmail.com>
2025-03-29 16:58:17 +0900

    Title: 체크판위에서 2, Time: 88ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250329/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,11 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+cnt = 0
+for i in range(1,n):
+    for j in range(1,m):
+        for k in range(i+1,n-1):
+            for l in range(j+1,m-1):
+                if grid[0][0] != grid[i][j] and grid[i][j] != grid[k][l] and grid[k][l] != grid[n-1][m-1]:
+                    cnt+=1 
+print(cnt)
